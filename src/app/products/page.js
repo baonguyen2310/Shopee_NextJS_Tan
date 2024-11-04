18287d32889eb62e63ac2f2c881cf363930d4b36
nhattan3007 <tnt30072006@gmail.com>
2024-11-04 15:50:57 +0700

    First-NextJs

---
+++ src/app/products/page.js
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const Products = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Products</h1>
+            <Footer />
+        </div>
+
+    )
+}
+
+export default Products
