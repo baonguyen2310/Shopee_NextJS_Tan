752d8a5914fc3e197536ed1f79f189f12fd2dcc8
nhattan3007 <tnt30072006@gmail.com>
2024-11-27 13:28:20 +0700

    bug06

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,25 @@ const Header = () => {
 
     // ? : là toán tử 3 ngôi (condition ? true : false)
     return (
-        <h1>
-            {user ? (
-                <>
-                    <p>Welcome, {user.name}</p>
-                    <button onClick={() => setUser(null)}>Logout</button>
-                </>
-            ) : (
-                <button onClick={() => setUser({ id: 1, name: 'John' })}>Login</button>
-            )}
-        </h1>
+        <div id='main'>
+            <div className='backGroundMain'>
+                <Header className='shopee-headder shoppe-header-sticky'>
+                    
+                </Header>
+            </div>
+            <h1>
+                {user ? (
+                    <>
+                        <p>Welcome, {user.name}</p>
+                        <button onClick={() => setUser(null)}>Logout</button>
+                    </>
+                ) : (
+                    <button onClick={() => setUser({ id: 1, name: 'John' })}>Login</button>
+                )}
+            </h1>
+        </div>
+
+
     )
 }
 
